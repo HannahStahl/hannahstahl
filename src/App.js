a102db8e09b5f8a23ecc5465084074a1a3e92306
Hannah Stahl <hls62@cornell.edu>
2019-11-18 09:35:55 -0600

    Added ESLint and fixed initial warnings

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ function App() {
   return (
     <div>
       <h1>
-        Hi, I'm Hannah!
+        {'Hi, I\'m Hannah!'}
       </h1>
-      <p>And I'd love to build you a stellar website.</p>
+      <p>{'And I\'d love to build you a stellar website.'}</p>
     </div>
   );
 }
