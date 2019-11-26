e5ef950a0d850ececb86df117ed591d5d40602cd
Hannah Stahl <hls62@cornell.edu>
2019-11-25 19:06:58 -0600

    Added portfolio items

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+const content = require('../content/portfolio.json');
+
 const Portfolio = () => (
   <div>
-    <h1>Portfolio Page</h1>
-    <p>Insert portfolio or products here.</p>
+    <div className="portfolio">
+      {content.portfolio.map((item) => (
+        <div className="portfolio-item" key={item.name}>
+          <a href={item.url} target="_blank" rel="noopener noreferrer">
+            <img src={`${item.img}.png`} alt={item.name} />
+          </a>
+        </div>
+      ))}
+    </div>
   </div>
 );
 
