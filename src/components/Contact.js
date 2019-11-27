cdc6dfeb50b2187961dd8be178d7cccf012f0186
Hannah Stahl <hls62@cornell.edu>
2019-11-27 12:59:01 -0500

    Filled in contact page with Calendly link and non-functional contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Contact = () => (
-  <div>
-    <h1>Contact Page</h1>
-    <p>Insert contact form here.</p>
+  <div className="contact">
+    <a href="https://calendly.com/hannahstahl/consultation" target="_blank" rel="noopener noreferrer">
+      <Button variant="outline-primary" size="lg">Schedule a free consultation</Button>
+    </a>
+    <div className="line-header">OR</div>
+    <h4 className="send-email-text">Send me an email:</h4>
+    <Form>
+      <Form.Group controlId="name">
+        <Form.Control type="text" placeholder="Your name" />
+      </Form.Group>
+      <Form.Group controlId="email">
+        <Form.Control type="email" placeholder="Your email address" />
+      </Form.Group>
+      <Form.Group controlId="message">
+        <Form.Control as="textarea" rows="5" placeholder="Your message" />
+      </Form.Group>
+      <Button variant="primary" type="submit" block>
+        Send
+      </Button>
+    </Form>
   </div>
 );
 
