bbb0f5f8911c124bebfd35b7fb788c81d26fb5eb
Hannah Stahl <hls62@cornell.edu>
2019-11-27 15:35:26 -0500

    Added functionality to contact form, and removed Olea Edge example from portfolio until it is live

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,29 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-const Contact = () => (
-  <div className="contact">
-    <a href="https://calendly.com/hannahstahl/consultation" target="_blank" rel="noopener noreferrer">
-      <Button variant="outline-primary" size="lg">Schedule a free consultation</Button>
-    </a>
-    <div className="line-header">OR</div>
-    <h4 className="send-email-text">Send me an email:</h4>
-    <Form>
-      <Form.Group controlId="name">
-        <Form.Control type="text" placeholder="Your name" />
-      </Form.Group>
-      <Form.Group controlId="email">
-        <Form.Control type="email" placeholder="Your email address" />
-      </Form.Group>
-      <Form.Group controlId="message">
-        <Form.Control as="textarea" rows="5" placeholder="Your message" />
-      </Form.Group>
-      <Button variant="primary" type="submit" block>
-        Send
-      </Button>
-    </Form>
-  </div>
-);
+export default class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '', email: '', message: '', sending: false, sent: false };
+  }
 
-export default Contact;
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { name, email, message } = this.state;
+    this.setState({ sending: true });
+    const url = process.env.REACT_APP_STAGE === 'prod'
+      ? 'https://3x7y40mgsh.execute-api.us-east-1.amazonaws.com/prod'
+      : 'https://m9nbwrw0ye.execute-api.us-east-1.amazonaws.com/dev';
+    fetch(`${url}/email/send`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name, email, message }),
+    }).then(response => response.json()).then((json) => {
+      if (json.MessageId) {
+        this.setState({ name: '', email: '', message: '', sending: false, sent: true });
+      } else {
+        window.alert('There was an error sending your message. Please email me directly at hannahstahl14@gmail.com. Thank you!');
+      }
+    });
+  }
+
+  handleChange = event => this.setState({ [event.target.id]: event.target.value, sent: false });
+
+  disabled = () => {
+    const { name, email, message, sending, sent } = this.state;
+    return sent || sending || name.length === 0 || email.length === 0 || message.length === 0;
+  }
+
+  buttonText = () => {
+    const { sending, sent } = this.state;
+    if (sent) return 'Sent!';
+    if (sending) return 'Sending...';
+    return 'Send';
+  }
+
+  render() {
+    const { name, email, message } = this.state;
+    return (
+      <div className="contact">
+        <a href="https://calendly.com/hannahstahl/consultation" target="_blank" rel="noopener noreferrer">
+          <Button variant="outline-primary" size="lg">Schedule a free consultation</Button>
+        </a>
+        <div className="line-header">OR</div>
+        <h4 className="send-email-text">Send me an email.</h4>
+        <p>I will get back to you within 1 business day.</p>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="name">
+            <Form.Control type="text" placeholder="Your name" value={name} onChange={this.handleChange} />
+          </Form.Group>
+          <Form.Group controlId="email">
+            <Form.Control type="email" placeholder="Your email address" value={email} onChange={this.handleChange} />
+          </Form.Group>
+          <Form.Group controlId="message">
+            <Form.Control as="textarea" rows="5" placeholder="Your message" value={message} onChange={this.handleChange} />
+          </Form.Group>
+          <Button variant="primary" type="submit" block disabled={this.disabled()}>
+            {this.buttonText()}
+          </Button>
+        </Form>
+      </div>
+    );
+  }
+}
