0e7b66633235498321882de2bd685f659fae78ac
Hannah Stahl <hls62@cornell.edu>
2019-12-08 15:20:15 -0600

    Delayed home content div display to allow photo to load

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons';
 
-const Home = () => (
-  <div className="home">
-    <div className="home-content">
-      <h1>Focus on what you do best.</h1>
-      <h3>Let me handle the website.</h3>
-      <a href="/#about"><FontAwesomeIcon icon={faAngleDoubleDown} className="down-arrow" /></a>
-    </div>
-  </div>
-);
+export default class Home extends Component {
+  componentDidMount() {
+    setTimeout(this.showDiv, 500);
+  }
 
-export default Home;
+  showDiv = () => {
+    document.getElementById('home-content').className = 'home-content visible';
+  }
+
+  render() {
+    return (
+      <div className="home">
+        <div id="home-content" className="home-content hidden">
+          <h1>Focus on what you do best.</h1>
+          <h3>Let me handle the website.</h3>
+          <a href="/#about"><FontAwesomeIcon icon={faAngleDoubleDown} className="down-arrow" /></a>
+        </div>
+      </div>
+    );
+  }
+}
