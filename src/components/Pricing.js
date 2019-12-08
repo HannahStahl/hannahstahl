acfaa99063dcb4d763e0ab485e8918c84d990c2c
Hannah Stahl <hls62@cornell.edu>
2019-12-08 14:23:35 -0600

    Added fade-in animations to pricing plans

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Fade } from 'react-reveal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle, faShoppingCart, faPlus } from '@fortawesome/free-solid-svg-icons';
 
@@ -19,21 +20,25 @@ const Pricing = () => (
         <ul>{content.maintenance.map((feature) => <li key={feature}><p>{feature}</p></li>)}</ul>
       </div>
     </div>
-    <div className="plans">
-      {content.plans.map((plan) => (
-        <div className="plan-container" key={plan.name}>
-          <div className="plan" style={{ backgroundColor: `var(--extra-light-${plan.color}` }}>
-            <div className="plan-icon">{icons[plan.icon]}</div>
-            <h2 className="plan-name">{plan.name}</h2>
-            <h3>{plan.price}</h3>
-            <h4 className="plan-description">{plan.description}</h4>
-            {plan.features && (
-              <ul>{plan.features.map((feature) => <li key={feature} className="plan-feature"><p>{feature}</p></li>)}</ul>
-            )}
-            {plan.details && <p className="plan-details">{plan.details}</p>}
-          </div>
+    <div>
+      <Fade bottom cascade>
+        <div className="plans">
+          {content.plans.map((plan) => (
+            <div key={plan.name} className="plan-container">
+              <div className="plan" style={{ backgroundColor: `var(--extra-light-${plan.color}` }}>
+                <div className="plan-icon">{icons[plan.icon]}</div>
+                <h2 className="plan-name">{plan.name}</h2>
+                <h3>{plan.price}</h3>
+                <h4 className="plan-description">{plan.description}</h4>
+                {plan.features && (
+                  <ul>{plan.features.map((feature) => <li key={feature} className="plan-feature"><p>{feature}</p></li>)}</ul>
+                )}
+                {plan.details && <p className="plan-details">{plan.details}</p>}
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      </Fade>
     </div>
   </div>
 );
