4390ab9f2e94ec8e070ab55b6c339c9b54ea6c20
Hannah Stahl <hls62@cornell.edu>
2019-12-08 12:53:26 -0600

    Fixed navbar style issues

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,8 @@ import Nav from 'react-bootstrap/Nav';
 
 const NavBar = () => {
   const section = window.location.hash;
-  const transparent = window.innerWidth > 992 && (section === '' || section === '#home' || section === '#contact');
-  return (
-    <Navbar collapseOnSelect expand="lg" fixed="top" className={transparent ? 'transparent' : ''}>
+  return (section === '' || section === '#' || section === '#home') ? <div /> : (
+    <Navbar collapseOnSelect expand="lg" fixed="top">
       <Navbar.Brand href="/#home">
         <img
           alt="Home"
@@ -18,7 +17,7 @@ const NavBar = () => {
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="ml-auto" activeKey={`/${window.location.hash}`}>
+        <Nav className="ml-auto" activeKey={`/${section}`}>
           <Nav.Link href="/#about">About</Nav.Link>
           <Nav.Link href="/#portfolio">Portfolio</Nav.Link>
           <Nav.Link href="/#pricing">Pricing</Nav.Link>
