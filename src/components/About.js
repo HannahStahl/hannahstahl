a0062624b46209b96735f5267f98d44c44764b57
Hannah Stahl <hls62@cornell.edu>
2019-12-08 14:08:29 -0600

    Added Fade animations to About sections

--- src/components/About.js
+++ src/components/About.js
@@ -1,28 +1,45 @@
 import React from 'react';
+import { Fade } from 'react-reveal';
 import config from '../config';
 
 const content = require('../content/about.json');
 
 const About = () => (window.innerWidth > 992 ? (
   <div className="about">
-    {content.map((section, index) => (
-      <div key={section.header} className="about-section">
-        {index % 2 === 0 && (
-          <div>
-            <img src={`${config.cloudFrontURL}/${section.photo}`} alt="About" className={`about-photo ${index === 2 ? 'cms' : ''}`} />
+    {content.map((section, index) => {
+      if (index % 2 === 0) {
+        return (
+          <Fade left key={section.header}>
+            <div className="about-section">
+              <div>
+                <img
+                  src={`${config.cloudFrontURL}/${section.photo}`}
+                  alt="About"
+                  className={`about-photo ${index === 2 ? 'cms' : ''}`}
+                />
+              </div>
+              <div className="about-section-text about-section-2">
+                <h1>{section.header}</h1>
+                <p>{section.paragraph}</p>
+              </div>
+            </div>
+          </Fade>
+        );
+      }
+      return (
+        <Fade right key={section.header}>
+          <div className="about-section">
+            <div className="about-section-text">
+              <h1>{section.header}</h1>
+              <p>{section.paragraph}</p>
+            </div>
+            <div className="about-section-2">
+              <img src={`${config.cloudFrontURL}/${section.photo}`} alt="About" className="about-photo" />
+            </div>
           </div>
-        )}
-        <div className={`about-section-text ${index % 2 === 0 && 'about-section-2'}`}>
-          <h1>{section.header}</h1>
-          <p>{section.paragraph}</p>
-        </div>
-        {index % 2 === 1 && (
-          <div className="about-section-2">
-            <img src={`${config.cloudFrontURL}/${section.photo}`} alt="About" className="about-photo" />
-          </div>
-        )}
-      </div>
-    ))}
+        </Fade>
+      );
+    })}
   </div>
 ) : (
   <div className="mobile-about">
