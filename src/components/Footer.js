5e0c4fcc670654b3dbf80aff8ba99f04c4bdeb56
Hannah Stahl <hls62@cornell.edu>
2020-03-10 09:09:54 -0500

    Changed copyright symbol

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer>
-    <div>
-      <img src="copyright.svg" alt="Copyright" width="20px" className="copyright-icon" />
-      Hannah Stahl, 2019. All Rights Reserved.
-    </div>
+    <div>© Hannah Stahl, 2019. All Rights Reserved.</div>
   </footer>
 );
 
