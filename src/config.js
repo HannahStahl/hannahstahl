38d2b2259461b4f92d33676ad13a142adf42976c
Hannah Stahl <hls62@cornell.edu>
2022-07-23 21:38:52 -0500

    Forgot to re-update config

--- src/config.js
+++ src/config.js
@@ -1,14 +1,4 @@
-const dev = {
-  emailServiceURL: 'https://m9nbwrw0ye.execute-api.us-east-1.amazonaws.com/dev',
-};
-
-const prod = {
-  emailServiceURL: 'https://3x7y40mgsh.execute-api.us-east-1.amazonaws.com/prod',
-};
-
-const config = process.env.REACT_APP_STAGE === 'prod' ? prod : dev;
-
 export default {
-  ...config,
   cloudFrontURL: 'http://d3me27l3ng8az4.cloudfront.net',
+  emailServiceURL: 'https://aiikn63n03.execute-api.us-east-1.amazonaws.com/prod/email/send',
 };
